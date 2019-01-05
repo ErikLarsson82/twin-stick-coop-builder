ee387c52b1be91c65d6ee3f10a2123cbd0b73bd1
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-01-05 20:23:52 +0100

    button and controller tweaks

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ const FPS = 60
 const delta = 1.0/FPS
 
 const buttonState = {
-    x: false
+    shoulder_top_right: false,
+    axis_left_x: null
 }
 
 window.addEventListener('load', init)
@@ -42,54 +43,61 @@ function init() {
 }
 
 function findName(name, world) {
-    /*var body = world.m_bodyList
-    while (body && body.name !== name) {
-        body = body.m_next
-    }
-    return body  */
-
     for (var bb = world.m_bodyList; bb; bb = bb.m_next){
         if (bb.name === name) return bb
     }
     return null
 }
 
-function gameTick() {
-    world.Step(delta, 4, 4);
+function updatePlayer() {
+    const playerBody = findName("player1", world)
+    if (!playerBody) return
 
-    if (buttonState.x) {
-        const playerBody = findName("player", world)
-        debugger
+    if (buttonState.shoulder_top_right) {
         playerBody.ApplyImpulse(new b2Vec2(0, 0.2), playerBody.GetPosition())
     }
+
+    if (buttonState.axis_left_x !== null) {
+        playerBody.ApplyImpulse(new b2Vec2(buttonState.axis_left_x / 10, 0), playerBody.GetPosition())
+    }
+}
+
+function gameTick() {
+    world.Step(delta, 4, 4)
+
+    updatePlayer()
 
     context.fillStyle = "lightgray"
     context.fillRect(0, 0, 1024, 768)
 
     context.save()
-    context.scale(1, -1);
-    world.DrawDebugData();
-    context.restore();
+    context.scale(1, -1)
+    world.DrawDebugData()
+    context.restore()
 }
 
 function initGamepad() {
     gamepad = new Gamepad()
 
-    gamepad.on('connect', e => {
-        console.log(`controller ${e.index} connected!`)
-    })
+    gamepad.on('connect', e => console.log(`controller ${e.index} connected!`))
 
-    gamepad.on('press', 'button_1', () => {
-        console.log('button 1 was pressed!')
-    })
+    gamepad.on('hold',    'shoulder_top_right ', () => buttonState.shoulder_top_right = true)
+    gamepad.on('release', 'shoulder_top_right ', () => buttonState.shoulder_top_right = false)
 
-    gamepad.on('hold', 'button_1', () => {
-        console.log('button 1 is being held!');
-        buttonState.x = true
-    });
+    gamepad.on('hold',    'stick_axis_left', e => buttonState.axis_left_x = e.value[0])
+    gamepad.on('release', 'stick_axis_left', () => buttonState.axis_left_x = null)
 
-    gamepad.on('release', 'button_1', () => {
-        console.log('button 1 was released!');
-        buttonState.x = false
-    });
+    /*
+    gamepad.on('hold', 'd_pad_left', () => buttonState.left = true)
+    gamepad.on('release', 'd_pad_left', () => buttonState.left = false)
+
+    gamepad.on('hold', 'd_pad_right', () => buttonState.right = true)
+    gamepad.on('release', 'd_pad_right', () => buttonState.right = false)
+
+    gamepad.on('hold', 'd_pad_left', () => buttonState.left = true)
+    gamepad.on('release', 'd_pad_left', () => buttonState.left = false)
+    */
+    //gamepad.on('hold', 'stick_axis_right', e => buttonState.axis_left = e.value[0])
+    //gamepad.on('release', 'd_pad_right', () => buttonState.right = false)
+
 }
