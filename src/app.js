ea18c9b79b52ec46e90995f6ea70573ed800d5fe
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-01-05 21:03:46 +0100

    Adding weird features

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const Box2D = require('./Box2D-module')
 const mapLoader = require('./mapLoader')
 const map = require('../json/map.json')
 const player = require('../json/player.json')
+const placer = require('../json/placer.json')
 
 var canvas, context, world, debugDraw, gamepad
 
@@ -11,8 +12,11 @@ const b2Vec2 = Box2D.Common.Math.b2Vec2
 const FPS = 60
 const delta = 1.0/FPS
 
-const buttonState = {
+const buttons = {
+    button_1: false,
+    shoulder_top_left: false,
     shoulder_top_right: false,
+    axis_right: null,
     axis_left_x: null
 }
 
@@ -22,20 +26,20 @@ function init() {
 
     initGamepad()
 
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
+    canvas = document.getElementById('canvas')
+    context = canvas.getContext('2d')
 
-    debugDraw = new Box2D.Dynamics.b2DebugDraw();
-    debugDraw.SetSprite(context);
-    debugDraw.SetDrawScale(45.0);
-    debugDraw.SetFillAlpha(0.3);
-    debugDraw.SetLineThickness(1.0);
-    debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_jointBit);
+    debugDraw = new Box2D.Dynamics.b2DebugDraw()
+    debugDraw.SetSprite(context)
+    debugDraw.SetDrawScale(45.0)
+    debugDraw.SetFillAlpha(0.3)
+    debugDraw.SetLineThickness(1.0)
+    debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_jointBit)
 
-    world = new Box2D.Dynamics.b2World(new b2Vec2(0, -4), true);
+    world = new Box2D.Dynamics.b2World(new b2Vec2(0, -4), true)
 
-    world.SetDebugDraw(debugDraw);
-    mapLoader(world, map);
+    world.SetDebugDraw(debugDraw)
+    mapLoader(world, map)
 
     mapLoader(world, player, {x: 6.5, y: -11})
 
@@ -49,16 +53,59 @@ function findName(name, world) {
     return null
 }
 
+function box2dToScreenPosition(pos) {
+    return {
+        x: pos.x * 45,
+        y: pos.y * -45
+    }
+}
+
+function rotate(angle, vec) {
+    return {
+        x: (Math.cos(angle) * vec.x) - (Math.sin(angle) * vec.y),
+        y: (Math.sin(angle) * vec.x) + (Math.cos(angle) * vec.y),
+    }
+}
+
+function sum(v1, v2) {
+    return {
+        x: v1.x + v2.x,
+        y: v1.y + v2.y
+    }
+}
+
+function aim(arr) {
+    return {
+        x: buttons.axis_right[0] * 2,
+        y: buttons.axis_right[1] * -2
+    }
+}
+
 function updatePlayer() {
     const playerBody = findName("player1", world)
     if (!playerBody) return
 
-    if (buttonState.shoulder_top_right) {
+    if (buttons.shoulder_top_left || buttons.button_1) {
         playerBody.ApplyImpulse(new b2Vec2(0, 0.2), playerBody.GetPosition())
     }
 
-    if (buttonState.axis_left_x !== null) {
-        playerBody.ApplyImpulse(new b2Vec2(buttonState.axis_left_x / 10, 0), playerBody.GetPosition())
+    if (buttons.shoulder_top_right && buttons.axis_right !== null) {
+        mapLoader(world, placer, sum(playerBody.GetPosition(), aim(buttons.axis_right)))
+        buttons.shoulder_top_right = false
+    }
+
+    if (buttons.axis_left_x !== null) {
+        playerBody.ApplyImpulse(new b2Vec2(buttons.axis_left_x / 10, 0), playerBody.GetPosition())
+    }
+}
+
+function drawPlayer() {
+    const playerBody = findName("player1", world)
+    if (buttons.axis_right !== null) {
+        const dot = sum(playerBody.GetPosition(), aim(buttons.axis_right))
+        const pos = box2dToScreenPosition(dot)
+        context.fillStyle = "red"
+        context.fillRect(pos.x, pos.y, 3, 3)
     }
 }
 
@@ -74,6 +121,8 @@ function gameTick() {
     context.scale(1, -1)
     world.DrawDebugData()
     context.restore()
+
+    drawPlayer()
 }
 
 function initGamepad() {
@@ -81,23 +130,32 @@ function initGamepad() {
 
     gamepad.on('connect', e => console.log(`controller ${e.index} connected!`))
 
-    gamepad.on('hold',    'shoulder_top_right ', () => buttonState.shoulder_top_right = true)
-    gamepad.on('release', 'shoulder_top_right ', () => buttonState.shoulder_top_right = false)
+    gamepad.on('hold',    'shoulder_top_left ', () => buttons.shoulder_top_left = true)
+    gamepad.on('release', 'shoulder_top_left ', () => buttons.shoulder_top_left = false)
 
-    gamepad.on('hold',    'stick_axis_left', e => buttonState.axis_left_x = e.value[0])
-    gamepad.on('release', 'stick_axis_left', () => buttonState.axis_left_x = null)
+    gamepad.on('hold',    'shoulder_top_right ', () => buttons.shoulder_top_right = true)
+    gamepad.on('release', 'shoulder_top_right ', () => buttons.shoulder_top_right = false)
+
+    gamepad.on('hold',    'button_1', () => buttons.button_1 = true)
+    gamepad.on('release', 'button_1', () => buttons.button_1 = false)
+
+    gamepad.on('hold',    'stick_axis_left', e => buttons.axis_left_x = e.value[0])
+    gamepad.on('release', 'stick_axis_left', () => buttons.axis_left_x = null)
+
+    gamepad.on('hold',    'stick_axis_right', e => buttons.axis_right = e.value)
+    gamepad.on('release', 'stick_axis_right', () => buttons.axis_right = null)
 
     /*
-    gamepad.on('hold', 'd_pad_left', () => buttonState.left = true)
-    gamepad.on('release', 'd_pad_left', () => buttonState.left = false)
+    gamepad.on('hold', 'd_pad_left', () => buttons.left = true)
+    gamepad.on('release', 'd_pad_left', () => buttons.left = false)
 
-    gamepad.on('hold', 'd_pad_right', () => buttonState.right = true)
-    gamepad.on('release', 'd_pad_right', () => buttonState.right = false)
+    gamepad.on('hold', 'd_pad_right', () => buttons.right = true)
+    gamepad.on('release', 'd_pad_right', () => buttons.right = false)
 
-    gamepad.on('hold', 'd_pad_left', () => buttonState.left = true)
-    gamepad.on('release', 'd_pad_left', () => buttonState.left = false)
+    gamepad.on('hold', 'd_pad_left', () => buttons.left = true)
+    gamepad.on('release', 'd_pad_left', () => buttons.left = false)
     */
-    //gamepad.on('hold', 'stick_axis_right', e => buttonState.axis_left = e.value[0])
-    //gamepad.on('release', 'd_pad_right', () => buttonState.right = false)
+    //gamepad.on('hold', 'stick_axis_right', e => buttons.axis_left = e.value[0])
+    //gamepad.on('release', 'd_pad_right', () => buttons.right = false)
 
 }
